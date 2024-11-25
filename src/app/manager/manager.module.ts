684e943e7c41ede538f67ef0a652c5064de2643a
Tien Phong <tienphong2203@gmail.com>
2024-11-25 20:18:12 +0700

    Step 8: Config recipe list and details

--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoryListComponent } from './category/category-list.component';
+import { RecipeListComponent } from './recipe/recipe-list.component';
 
 const routes: Routes = [
   {
     path: 'categories',
     component: CategoryListComponent
   },
-
+  {
+    path: 'recipes',
+    component: RecipeListComponent
+  },
   {
     path: '**',
     redirectTo: 'categories',
