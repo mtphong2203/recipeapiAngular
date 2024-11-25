f6efe30333f603268ba5190d90354f8beffe7756
Tien Phong <tienphong2203@gmail.com>
2024-11-25 21:22:29 +0700

    Step 10: Call API from backend base github users

--- src/app/manager/category/category-list.component.ts
+++ src/app/manager/category/category-list.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 @Component({
   selector: 'app-category-list',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './category-list.component.html',
   styleUrl: './category-list.component.css'
 })
-export class CategoryListComponent {
+export class CategoryListComponent implements OnInit {
+  private apiURL = 'https://api.github.com/users';
+  public data: any[] = [];
 
+  constructor(private http: HttpClient) { }
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  private getData(): void {
+    this.http.get(this.apiURL).subscribe((data) => {
+      this.data = data as any[];
+    });
+  }
 }
